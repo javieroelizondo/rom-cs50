84b6768b1026e7f2ad55a8719fbc3a7176bac88a
Javier Elizondo <javiero.elizondo@gmail.com>
2025-02-28 17:31:33 +1300

    v0: First commit. Initial setup: Flask app, SQLite database, and User model

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from models import User
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///rigops.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+@app.route('/')
+def home():
+    return "Welcome to RigOps Manager!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
